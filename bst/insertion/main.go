91dde3ea1a087eee44b945a81a20f490df0881cc
yassine benlamkadem <yassine.benlamkadem@gmail.com>
2025-05-26 20:24:34 +0100

    bst-insert

--- bst/insertion/main.go
+++ bst/insertion/main.go
@@ -1,1 +1,38 @@
+package bst
+
+import (
+	"dsa/bst/bst-sorted-list"
+)
+
+func AddNodeTree(t *bst.BST, m int) {
+	if t.Root == nil {
+		t.Root = &bst.Node{
+			Val: m,
+		}
+	} else {
+		AddNode(t.Root, m)
+	}
+}
+
+func AddNode(n *bst.Node, m int) {
+	if m < n.Val && n.Left != nil {
+		AddNode(n.Left, m)
+	}
+	if m < n.Val && n.Left == nil {
+		n.Left = &bst.Node{
+			Val: m,
+		}
+	}
+	if m >= n.Val && n.Right != nil {
+		AddNode(n.Right, m)
+	}
+	if m >= n.Val && n.Right == nil {
+		n.Right = &bst.Node{
+			Val: m,
+		}
+	}
+}
+
 // Insert n values into a BST while ensuring BST properties are preserved
+// The idea here is to traverse the current tree and insert the node where appropriate by comparing the value we are inserting with the current node
+// then we recurse down the tree as necessary until we find the correct place for the value
--- bst/main.go
+++ bst/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"dsa/bst/bst-sorted-list"
+	add "dsa/bst/insertion"
 	"fmt"
 )
 
 func main() {
-	TestBSTFromSlc([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-	TestBSTFromSlc([]int{1, 2, 3, 4, 5, 6, 7})
-	TestBSTFromSlc([]int{1, 2, 3, 4})
-	TestBSTFromSlc([]int{0, 1, 2, 7, 8})
-	TestBSTFromSlc([]int{10})
+	// TestBSTFromSlc([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	// TestBSTFromSlc([]int{1, 2, 3, 4, 5, 6, 7})
+	// TestBSTFromSlc([]int{1, 2, 3, 4})
+	// TestBSTFromSlc([]int{0, 1, 2, 7, 8})
+	// TestBSTFromSlc([]int{10})
+
+	TestAddNode([]int{6, 4, 9, 3, 7, 8, 2, 5, 1, 10})
+	TestAddNode([]int{4, 5, 1, 2, 6, 7, 3})
+	TestAddNode([]int{0, 3, 4, 2, 1})
+	TestAddNode([]int{7, 1, 0, 7, 8, 2})
+	TestAddNode([]int{10})
+	TestAddNode([]int{6, 4, 2, 5, 1, 3, 8, 7, 9, 10})
 }
 
 func TestBSTFromSlc(arr []int) {
 	t := bst.BSTFromSlc(arr)
+	fmt.Println("-------------------------------------------------------------------------------")
+	t.PrintCompBinTree()
+	fmt.Println()
+}
+
+func TestAddNode(arr []int) {
+	t := &bst.BST{}
+	for _, e := range arr {
+		add.AddNodeTree(t, e)
+	}
 	fmt.Println("-------------------------------------------------------------------------------")
 	t.PrintCompBinTree()
 	fmt.Println()
